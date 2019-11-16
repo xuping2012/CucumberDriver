cfc1125ed0b89bdfcecc5f8e50d11537e7537b5a
studyWorkSpace <oupoor@qq.com>
2019-11-16 12:40:32 +0800

    update

--- src/com/cucumber/utilities/Constants.java
+++ src/com/cucumber/utilities/Constants.java
@@ -2,6 +2,6 @@ package com.cucumber.utilities;
 
 public class Constants {
 	public static String  account="125197291@qq.com";
-	public static String  password = "ZTEu807Q";
+	public static String  password = "sdfff";
 	public static String baseUrl = "https://mail.qq.com/cgi-bin/loginpage";
 }
